37da6a423ef0688c824d54e503638b9254a47b7a
Tim Stibbs <tstibbs@users.noreply.github.com>
2021-10-18 21:06:29 +0100

    Adding link to logs from notification emails

--- aws/environment-setup/src/utils.js
+++ aws/environment-setup/src/utils.js
@@ -26,12 +26,12 @@ export async function inSeries(things, executor) {
 }
 
 export function buildHandler(checkOneAccount, summarise) {
-	const runAllChecks = async (invocationId) => {
+	const runAllChecks = async invocationId => {
 		console.log(`invocationId=${invocationId}`) //just to make it easy to match up an email and a log entry
 		let allAcountsData = await inSeries(childAccounts, async childAccount => await checkOneAccount(childAccount))
 		await summarise(invocationId, allAcountsData)
 	}
-	
+
 	const handleEvent = async (event, context) => {
 		let invocationId = context.awsRequestId
 		try {
@@ -50,12 +50,20 @@ export function buildHandler(checkOneAccount, summarise) {
 export async function publishNotification(message, title, invocationId) {
 	await sns
 		.publish({
-			Message: `${message}\n\ninvocationId=${invocationId}`,
+			Message: `${message}\n\ninvocationId=${invocationId}\n\n${getCurrentLambdaLogsLink()}`,
 			TopicArn: alertsTopic,
 			Subject: title
 		})
 		.promise()
 	console.log('published sns alert')
+}
+
+function getCurrentLambdaLogsLink() {
+	const {AWS_REGION, AWS_LAMBDA_LOG_GROUP_NAME, AWS_LAMBDA_LOG_STREAM_NAME} = process.env
+	const encode = input => encodeURIComponent(encodeURIComponent(input)).replace(/%/g, '$')
+	return `https://${AWS_REGION}.console.aws.amazon.com/cloudwatch/home?region=${AWS_REGION}#logsV2:log-groups/log-group/${encode(
+		AWS_LAMBDA_LOG_GROUP_NAME
+	)}/log-events/${encode(AWS_LAMBDA_LOG_STREAM_NAME)}`
 }
 
 export async function buildApiForAccount(accountId, api) {
