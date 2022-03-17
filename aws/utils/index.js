073c8a734a606935161eefc98411f6cc7126dee6
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-03-17 21:24:12 +0000

    Adding reuseable 'website' stack resources

--- aws/utils/index.js
+++ aws/utils/index.js
@@ -1,5 +1,6 @@
 import {loadEnvs, ifCmd, exec} from './utils.js'
 import {validateBuiltAssets as validateCdkAssets} from './src/cdkTools/buildTests.js'
 import {checkAllStackPolicies} from './src/cdkTools/stackTests.js'
+import {buildWebsiteResources} from './src/stacks/website.js'
 
-export {loadEnvs, ifCmd, exec, validateCdkAssets, checkAllStackPolicies}
+export {loadEnvs, ifCmd, exec, validateCdkAssets, checkAllStackPolicies, buildWebsiteResources}
---
+++ aws/utils/src/stacks/website.js
@@ -0,0 +1,24 @@
+import { RemovalPolicy, CfnOutput } from '@aws-cdk/core'
+import { Distribution, GeoRestriction, ResponseHeadersPolicy } from '@aws-cdk/aws-cloudfront'
+import { S3Origin } from '@aws-cdk/aws-cloudfront-origins'
+import { Bucket } from '@aws-cdk/aws-s3'
+
+export function buildWebsiteResources(stack, bucketResourceName, denyCountries) {
+	const bucket = new Bucket(stack, bucketResourceName, {
+		removalPolicy: RemovalPolicy.DESTROY,
+		autoDeleteObjects: true
+	})
+
+	const distributionProps = {
+		defaultBehavior: {
+			origin: new S3Origin(bucket),
+			responseHeadersPolicy: ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT
+		}
+	}
+	if (denyCountries != null && denyCountries.length > 0) {
+		distributionProps.geoRestriction = GeoRestriction.denylist(...denyCountries)
+	}
+	const distribution = new Distribution(stack, 'distribution', distributionProps)
+
+	new CfnOutput(stack, 'distributionDomainName', { value: distribution.distributionDomainName })
+}
