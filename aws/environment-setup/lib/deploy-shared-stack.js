8d28060a319d5a45138fe4c3703f3f341d093888
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-07-07 21:21:51 +0100

    Added usage monitoring to exposed APIs

--- aws/environment-setup/lib/deploy-shared-stack.js
+++ aws/environment-setup/lib/deploy-shared-stack.js
@@ -1,17 +1,19 @@
-import cdk from 'aws-cdk-lib'
-import iam from 'aws-cdk-lib/aws-iam'
+import {Stack, CfnOutput} from 'aws-cdk-lib'
+import {Role, CompositePrincipal, ServicePrincipal, ArnPrincipal, ManagedPolicy} from 'aws-cdk-lib/aws-iam'
 
 import {applyStandardTags} from '@tstibbs/cloud-core-utils'
+import {apiGatewayCloudwatchRoleRef} from '@tstibbs/cloud-core-utils/src/stacks/usage-tracking.js'
 
 import {buildDeveloperPolicy, buildCloudFormationInvokerPolicy, buildScoutSuitePolicy} from './deploy-shared-roles.js'
 import {PARENT_ACCOUNT_ID} from './deploy-envs.js'
 import {PARENT_ACCNT_CLI_ROLE_NAME} from './deploy-shared.js'
 
-class AllAccountsStack extends cdk.Stack {
+class AllAccountsStack extends Stack {
 	constructor(scope, id, props) {
 		super(scope, id, props)
 		createCliRoles(this)
 		createScoutSuiteElements(this)
+		createApplicationDependencies(this)
 		applyStandardTags(this)
 	}
 }
@@ -19,15 +21,15 @@ class AllAccountsStack extends cdk.Stack {
 function createCliRoles(stack) {
 	let developerPolicy = buildDeveloperPolicy(stack)
 	let cloudFormationInvokerPolicy = buildCloudFormationInvokerPolicy(stack)
-	new iam.Role(stack, 'parentAccountCliRole', {
+	new Role(stack, 'parentAccountCliRole', {
 		roleName: PARENT_ACCNT_CLI_ROLE_NAME,
-		assumedBy: new iam.CompositePrincipal(
-			new iam.ServicePrincipal('cloudformation.amazonaws.com', {
+		assumedBy: new CompositePrincipal(
+			new ServicePrincipal('cloudformation.amazonaws.com', {
 				assumeRoleAction: 'sts:AssumeRole'
 			}),
 			//Note that if the parent account core stack is dropped and recreated, these trust relationships will have to be recreated too (see https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_principal.html / IAM roles / Important)
-			new iam.ArnPrincipal(`arn:aws:iam::${PARENT_ACCOUNT_ID}:user/AllAccountCliEntryUser`),
-			new iam.ArnPrincipal(`arn:aws:iam::${PARENT_ACCOUNT_ID}:role/toolingFunctionsRole`)
+			new ArnPrincipal(`arn:aws:iam::${PARENT_ACCOUNT_ID}:user/AllAccountCliEntryUser`),
+			new ArnPrincipal(`arn:aws:iam::${PARENT_ACCOUNT_ID}:role/toolingFunctionsRole`)
 		),
 		managedPolicies: [developerPolicy, cloudFormationInvokerPolicy]
 	})
@@ -35,11 +37,25 @@ function createCliRoles(stack) {
 
 function createScoutSuiteElements(stack) {
 	let scoutSuitePolicy = buildScoutSuitePolicy(stack)
-	new iam.Role(stack, 'scoutSuiteRole', {
+	new Role(stack, 'scoutSuiteRole', {
 		roleName: 'ScoutSuiteRole',
 		//Note that if the parent account core stack is dropped and recreated, this trust relationship will have to be recreated too (see https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_principal.html / IAM roles / Important)
-		assumedBy: new iam.ArnPrincipal(`arn:aws:iam::${PARENT_ACCOUNT_ID}:user/AllAccountCliEntryUser`),
+		assumedBy: new ArnPrincipal(`arn:aws:iam::${PARENT_ACCOUNT_ID}:user/AllAccountCliEntryUser`),
 		managedPolicies: [scoutSuitePolicy]
+	})
+}
+
+function createApplicationDependencies(stack) {
+	//create a role for use by api gateway logging - create it once here because otherwise it'll get removed when we remove the stack that created it
+	const apiGatewayCloudWatchRole = new Role(stack, 'apiGatewayCloudWatchRole', {
+		assumedBy: new ServicePrincipal('apigateway.amazonaws.com', {
+			assumeRoleAction: 'sts:AssumeRole'
+		}),
+		managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName(`service-role/AmazonAPIGatewayPushToCloudWatchLogs`)]
+	})
+	new CfnOutput(stack, 'apiGatewayCloudWatchRoleArn', {
+		value: apiGatewayCloudWatchRole.roleArn,
+		exportName: apiGatewayCloudwatchRoleRef
 	})
 }
 
---
+++ aws/utils/src/stacks/usage-tracking.js
@@ -0,0 +1,32 @@
+import {RemovalPolicy} from 'aws-cdk-lib'
+import {LogGroup} from 'aws-cdk-lib/aws-logs'
+
+export const apiGatewayCloudwatchRoleRef = `AllAccountsStack-apiGatewayCloudWatchRoleArn`
+
+const standardLogFormat = '$context.identity.sourceIp,$context.httpMethod $context.path,$context.requestId'
+const websocketLogFormat = '$context.identity.sourceIp,$context.eventType $context.routeKey,$context.requestId'
+
+export function addUsageTrackingToHttpApi(httpApi) {
+	//workaround for https://github.com/aws/aws-cdk/issues/11100
+	httpApi.defaultStage.node.defaultChild.accessLogSettings = buildAccessLogSetting(httpApi, standardLogFormat)
+}
+
+export function addUsageTrackingToRestApi(restApi) {
+	restApi.deploymentStage.node.defaultChild.accessLogSetting = buildAccessLogSetting(restApi, standardLogFormat)
+}
+
+export function addUsageTrackingToWebsocketStage(webSocketStage) {
+	webSocketStage.node.defaultChild.accessLogSettings = buildAccessLogSetting(webSocketStage, websocketLogFormat)
+}
+
+function buildAccessLogSetting(parent, format) {
+	const {stack} = parent
+	const parentId = parent.node.id
+	const logGroup = new LogGroup(stack, `${parentId}-AccessLogs`, {
+		removalPolicy: RemovalPolicy.DESTROY
+	})
+	return  {
+		destinationArn: logGroup.logGroupArn,
+		format: format,
+	}
+} 
