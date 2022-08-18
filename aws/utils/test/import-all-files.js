3be0dfc1f21d645df93be9904008698af834c5d8
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-08-18 22:42:12 +0100

    Testing utils module in CI build

--- aws/utils/test/import-all-files.js
+++ aws/utils/test/import-all-files.js
@@ -13,4 +13,4 @@ for (let file of files) {
 	await import(file)
 }
 process.exitCode = 0 //because cli sets the exit code
-assert.equal(files.length, 7) //sanity check, update if the number of files changes
+assert.equal(files.length, 10) //sanity check, update if the number of files changes
