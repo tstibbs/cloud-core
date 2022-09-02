ebdc0f686df40a3c62578a9905bfff960c655fc3
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-09-02 19:36:19 +0100

    Usage monitor: correctly invoking lambda from local run script

--- aws/environment-setup/test/run-usage-monitor.js
+++ aws/environment-setup/test/run-usage-monitor.js
@@ -1,17 +1,3 @@
-import {processResources} from '../src/usage-monitor.js'
-import {USAGE_TYPE_LOG_GROUP} from '../src/constants.js'
+import {handler} from '../src/usage-monitor.js'
 
-const stacks = [
-	{
-		name: '<stack name>',
-		resources: [
-			{
-				name: '<name of resource that the records relate to>',
-				source: '<actual materialised resource name, e.g. log group name>',
-				type: USAGE_TYPE_LOG_GROUP
-			}
-		]
-	}
-]
-const nowDate = new Date('2022-08-25T00:00:00.000Z')
-await processResources('dummy', nowDate, stacks)
+await handler({}, {awsRequestId: 'dummy'})
