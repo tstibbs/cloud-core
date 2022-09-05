5903bff3c1a812fa8582c9c9ef5932ed972c1595
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-09-05 21:22:45 +0100

    Stopping cloudwatch query wasting time and erroring if all events post-date the end time of the query

--- aws/environment-setup/src/usage-utils-logs.js
+++ aws/environment-setup/src/usage-utils-logs.js
@@ -1,39 +1,69 @@
+import assert from 'assert'
+
 import backOff from 'exponential-backoff'
 
-import {cloudWatchLogs} from './utils.js'
+import {cloudWatchLogs, assertNotPaging} from './utils.js'
 
 const QUERY_COMPLETE = 'Complete'
 const QUERY = `fields @timestamp, @message
 | parse @message "*,* *,*" as @sourceIp, @method, @path
 | stats count(*) as count by @sourceIp as sourceIp, @method as method, @path as path, datefloor(@timestamp, 1d) as date`
 
-export async function fetchLogEntries(dates, logGroup) {
-	let {queryId} = await cloudWatchLogs
-		.startQuery({
-			queryString: QUERY,
-			endTime: dates.endTime,
-			startTime: dates.startTime,
-			logGroupName: logGroup
+/* if the endTime of the query is before the group creation time, or if retention settings 
+mean it will have nothing in, then it will error. Check here so we can return zero results 
+instead of trying to make an invalid query and then trying to catch the error. */
+async function isQueryInTimeRange(endTime, logGroupName) {
+	let endTimeInMillis = endTime * 1000
+	let result = await cloudWatchLogs
+		.describeLogGroups({
+			logGroupNamePrefix: logGroupName
 		})
 		.promise()
+	assertNotPaging(result)
+	assert.strictEqual(result.logGroups.length, 1, `Expected exactly a single log group with this name`)
+	let logGroup = result.logGroups[0]
+	let creationTimeInMillis = logGroup.creationTime
+	let {retentionInDays} = logGroup
+	//check creation time
+	let sinceCreation = endTimeInMillis >= creationTimeInMillis
+	//check retention
+	const oldestRetainedEntry = new Date()
+	oldestRetainedEntry.setUTCDate(oldestRetainedEntry.getUTCDate() - retentionInDays)
+	let sinceRetention = endTimeInMillis >= oldestRetainedEntry.getTime()
+	return sinceCreation && sinceRetention
+}
 
-	//keep checking the status of the query until it's completed
-	const backoffParams = {
-		maxDelay: 60 * 1000, // 1 minute
-		startingDelay: 2 * 1000 // 2 seconds
-	}
-	const checkForCompletion = async () => {
-		let queryResponse = await cloudWatchLogs
-			.getQueryResults({
-				queryId: queryId
+export async function fetchLogEntries(dates, logGroup) {
+	if ((await isQueryInTimeRange(dates.endTime, logGroup)) == false) {
+		return []
+	} else {
+		let {queryId} = await cloudWatchLogs
+			.startQuery({
+				queryString: QUERY,
+				endTime: dates.endTime,
+				startTime: dates.startTime,
+				logGroupName: logGroup
 			})
 			.promise()
-		if (queryResponse.status != QUERY_COMPLETE) {
-			throw new Error(`status=${queryResponse.status}`) // throwing error causes backoff to retry, though if not 'running' or 'scheduled' then it's unlikely to change
-		} else {
-			return queryResponse.results
+
+		//keep checking the status of the query until it's completed
+		const backoffParams = {
+			maxDelay: 60 * 1000, // 1 minute
+			startingDelay: 2 * 1000 // 2 seconds
 		}
+		const checkForCompletion = async () => {
+			let queryResponse = await cloudWatchLogs
+				.getQueryResults({
+					queryId: queryId
+				})
+				.promise()
+			if (queryResponse.status != QUERY_COMPLETE) {
+				throw new Error(`status=${queryResponse.status}`) // throwing error causes backoff to retry, though if not 'running' or 'scheduled' then it's unlikely to change
+			} else {
+				return queryResponse.results
+			}
+		}
+		let results = await backOff.backOff(checkForCompletion, backoffParams)
+		return results
 	}
-	let results = await backOff.backOff(checkForCompletion, backoffParams)
-	return results
 }
