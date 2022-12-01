8061e003f09d0ec214f9a2a653957310f0cb2e61
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-12-01 21:13:47 +0000

    Template configurability for whether errors are expected or not

--- ui/build/templates.js
+++ ui/build/templates.js
@@ -1,18 +1,18 @@
-import {fileURLToPath} from 'url'
 import ejs from 'ejs'
 import {readFile} from 'fs/promises'
 
-const __filename = fileURLToPath(import.meta.url)
-
-//by default filePathForRelativeIncludes assumes everything is relative to the current script
-export function renderTemplateContents(templateContents, vars, filePathForRelativeIncludes = __filename) {
+export function renderTemplateContents(templateContents, vars, filePathForRelativeIncludes) {
 	let template = ejs.render(templateContents, vars, {
 		filename: filePathForRelativeIncludes
 	})
 	return template
 }
 
-export async function renderTemplateFromFile(templateFilePath, vars, filePathForRelativeIncludes = __filename) {
-	let templateContents = await readFile(templateFilePath)
-	return renderTemplateContents(templateContents, vars, filePathForRelativeIncludes)
+export async function renderTemplateFromFile(templateFilePath, vars) {
+	let templateContents = (await readFile(templateFilePath)).toString()
+	return renderTemplateContents(templateContents, vars, templateFilePath)
+}
+
+export function buildTemplateContentRenderingFunction(templateFilePath, vars) {
+	return async ({htmlWebpackPlugin}) => await renderTemplateFromFile(templateFilePath, {...vars, htmlWebpackPlugin})
 }
