bb2375b1b0d9e7ede1464c57c439f1f2611ab08c
Tim Stibbs <tstibbs@users.noreply.github.com>
2023-02-24 10:50:11 +0000

    s3 index: updating tests

--- aws/utils/test/import-all-files.js
+++ aws/utils/test/import-all-files.js
@@ -3,6 +3,8 @@ import {fileURLToPath} from 'url'
 import {list} from 'recursive-readdir-async'
 import {strict as assert} from 'assert'
 
+const expectedNumberOfFiles = 13 //sanity check in case a change happens which breaks this test; update this if the number of files changes
+
 const dir = join(dirname(fileURLToPath(import.meta.url)), '../src')
 let files = await list(dir)
 assert.equal(files.error, undefined)
@@ -10,7 +12,7 @@ files = files
 	.filter(file => file.name.endsWith('.js') || file.name.endsWith('.mjs') || file.name.endsWith('.cjs'))
 	.map(file => file.fullname)
 console.log(files)
-assert.equal(files.length, 12) //sanity check in case a change happens which breaks this test; update this if the number of files changes
+assert.equal(files.length, expectedNumberOfFiles)
 for (let file of files) {
 	console.log(file)
 	await import(file)
