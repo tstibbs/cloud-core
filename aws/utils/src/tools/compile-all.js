f92e00c1a8ae503ce721a70073930178836ecbe9
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-12-01 21:07:00 +0000

    compile-all now takes account of .gitignore files

--- aws/utils/src/tools/compile-all.js
+++ aws/utils/src/tools/compile-all.js
@@ -1,14 +1,15 @@
 //just import everything and check there are no errors - no value in testing beyond this
 
 import {strict as assert} from 'node:assert'
-import {readdir} from 'node:fs/promises'
-import {join, extname, resolve} from 'node:path'
+import {join, resolve} from 'node:path'
+
+import {simpleGit} from 'simple-git'
 
 export async function importAll(meta, dirPath) {
 	dirPath = join(meta.dirname, dirPath)
 	console.log(`Checking ${dirPath}`)
 	const oldExitCode = process.exitCode //cache exit code in case one of the files we load sets an exit code
-	const files = (await readdir(dirPath, {recursive: true})).filter(file => extname(file) === '.js')
+	const files = await listFiles(dirPath)
 	const failures = []
 	for (let file of files) {
 		console.log(file)
@@ -23,4 +24,19 @@ export async function importAll(meta, dirPath) {
 	process.exitCode = oldExitCode
 	console.log(`\nFinished checking ${files.length} files.\n`)
 	assert.equal(failures.length, 0, `import failures happened: ${failures.join(', ')}`)
+}
+
+async function listFiles(dirPath) {
+	//ask git to list the files, so we can take advantage of its handling of .gitignore files
+	const git = simpleGit(dirPath)
+	const commandArgs = [
+		'ls-files',
+		'--exclude-standard', // ignore everything ignored by '.gitignore'
+		'--cached', // include tracked files
+		'--others', // include untracked files
+		'*.js',
+		'*.mjs'
+	]
+	const fileList = await git.raw(commandArgs)
+	return fileList.trim().split('\n')
 }
