637577bd084768cd4e385980859cd056968327cb
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-12-18 20:13:40 +0000

    utils: removing assumption that stack has been synthed before running tests

--- aws/environment-setup/test/test-built.js
+++ aws/environment-setup/test/test-built.js
@@ -6,14 +6,10 @@ import {
 	buildIotStack
 } from '../lib/deploy-entrypoint.js'
 
-let allAccountsStack = buildAllAccountsStack()
-await validateCdkAssets(allAccountsStack.stackName, 2) //s3 bucket emptier (shared accross buckets) + emergency tear down function
+await validateCdkAssets(buildAllAccountsStack, 2) //s3 bucket emptier (shared across buckets) + emergency tear down function
 
-let parentAccountCoreStack = buildParentAccountCoreStack()
-await validateCdkAssets(parentAccountCoreStack.stackName, 0)
+await validateCdkAssets(buildParentAccountCoreStack, 0)
 
-let parentAccountInfraStack = buildParentAccountInfraStack()
-await validateCdkAssets(parentAccountInfraStack.stackName, 5) //s3 bucket emptier + cloudformation drift checker + login checker + iam permissions checker + usage monitor
+await validateCdkAssets(buildParentAccountInfraStack, 5) //s3 bucket emptier + cloudformation drift checker + login checker + iam permissions checker + usage monitor
 
-let iotStack = buildIotStack()
-await validateCdkAssets(iotStack.stackName, 1) //uptimeCheckerFunction
+await validateCdkAssets(buildIotStack, 1) //uptimeCheckerFunction
